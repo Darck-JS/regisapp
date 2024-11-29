16c6863b6f94ba61e6aa9af4fdf8e29db16054e6
Darck-JS <jor.solis@duocuc.cl>
2024-11-29 03:00:15 -0300

    integrado api de camara

--- src/app/foto-registro/foto-registro.module.ts
+++ src/app/foto-registro/foto-registro.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { FotoRegistroPageRoutingModule } from './foto-registro-routing.module';
 
 import { FotoRegistroPage } from './foto-registro.page';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { FotoRegistroPage } from './foto-registro.page';
     IonicModule,
     FotoRegistroPageRoutingModule
   ],
-  declarations: [FotoRegistroPage]
+  declarations: [FotoRegistroPage, BarcodeScanningModalComponent]
 })
 export class FotoRegistroPageModule {}
--- src/app/foto-registro/foto-registro.page.ts
+++ src/app/foto-registro/foto-registro.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, Platform } from '@ionic/angular';
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { LensFacing, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-foto-registro',
@@ -12,14 +14,26 @@ export class FotoRegistroPage implements OnInit {
 
   curso = this.router.getCurrentNavigation()?.extras.state?.['curso'];
   usuario = this.router.getCurrentNavigation()?.extras.state?.['user'];
+  resultadoQR = '';
 
-  constructor(private activerouter: ActivatedRoute, private router: Router, private alertController: AlertController) {
+
+  constructor(private activerouter: ActivatedRoute, 
+              private router: Router, 
+              private alertController: AlertController, 
+              private modalController: ModalController,
+              private platform: Platform,
+              private controladorDeCarga: LoadingController) {
     this.activerouter.queryParams.subscribe(params => {
 
     });
   }
 
   ngOnInit() {
+    if (this.platform.is('capacitor')) {
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners();
+    }
   }
 
 
@@ -50,5 +64,44 @@ async takePicture() {
   const imageUri = image.webPath;
   this.imageSrc = imageUri;
 }
+
+
+
+
+
+async scannerQR() {
+  const modal = await this.modalController.create({
+  component: BarcodeScanningModalComponent,
+  cssClass: 'barcode-scanning-modal',
+  showBackdrop: false,
+  componentProps: { 
+    datos: [],
+    LensFacing: LensFacing.Back
+  }
+  });
+
+  await modal.present();
+
+
+  const { data } = await modal.onWillDismiss();
+
+  if(data){
+    this.resultadoQR = data?.barcode?.displeyValue;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
